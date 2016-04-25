99357ca1979c29db0903dc15a0ff291b83aa73d9
sf-testing <sforce@longbo.at>
2016-04-25 16:35:08 -0400

    Update app.rb

--- app.rb
+++ app.rb
@@ -17,9 +17,9 @@ get "/" do
 end
 
 
-class Contact < ActiveRecord::Base
-  self.table_name = 'salesforce.contact'
-end
+#class Contact < ActiveRecord::Base
+#  self.table_name = 'salesforce.contact'
+#end
 
 #get "/contacts" do
 #  @contacts = Contact.all
